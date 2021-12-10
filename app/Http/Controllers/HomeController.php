bffb78f88d0a8a462995ebd7088c3948405279ff
Yulia N <iuliia_nak@gm.com>
2021-12-11 00:01:19 +0200

    home page controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController
+{
+    public function index()
+    {
+        return '<h1>Добро пожаловать на домашнюю страницу</h1>';
+    }
+
+
+}
